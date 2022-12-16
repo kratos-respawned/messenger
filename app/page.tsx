0377a4a0a2f4930f13a987d72320e70eea54b23b
Gaurav Bhandari <sbhandari1907@protonmail.com>
2022-12-16 15:33:56 +0530

    ssr fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
-import React from "react";
 import { message } from "../typings";
 import ChatInput from "./ChatInput";
 import MessageList from "./MessageList";
 import Providers from "./Provider";
 import { unstable_getServerSession } from "next-auth/next";
-async function page() {
+async function getData() {
   const data = await fetch(`${process.env.VERCEL_URL}/api/getMessages`).then(
     (res) => res.json()
   );
+  return data;
+}
+async function page() {
+  const data = await getData();
+
   const session = await unstable_getServerSession();
   const messages: message[] = data.messages;
   return (
